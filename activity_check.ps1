d4cff8460e4f45c99cc26bca8ac0c0042bdc7c6a
secondlathloothy <67779412+secondlathloothy@users.noreply.github.com>
2024-09-08 20:41:05 +0300

    Update activity_check.ps1

--- activity_check.ps1
+++ activity_check.ps1
@@ -1,6 +1,6 @@
 # Define your webhook URL and the message content
 $webhookUrl = "https://discord.com/api/webhooks/1282390179981689064/U_QuvqQdGWdCA29tlsvg2Q5pK2pZNvWh1dbPeLLkryxrOFJ8BDR79ojicbbDxytJBSJ2"
-$message = "**# ACTIVITY CHECK**`n`nDay 1`n`nGoal: 9 reacts`n`n@everyone"
+$message = "**# ACTIVITY CHECK**`n`nDay 1`n`nGoal: 10 reacts`n`n@everyone"
 
 # Prepare the JSON payload
 $jsonPayload = @{
